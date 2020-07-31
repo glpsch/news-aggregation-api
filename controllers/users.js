cb3da657c64281b63bceeb9f567108f125437b81
Galina Psch <over.passover@gmail.com>
2020-07-31 23:47:19 +0300

    omit private

--- controllers/users.js
+++ controllers/users.js
@@ -59,7 +59,7 @@ module.exports.login = (req, res, next) => {
           httpOnly: true,
           sameSite: true,
         })
-        .send({ user });
+        .send({ data: user.omitPrivate() });
     })
     .catch((err) => {
       let error = err;
