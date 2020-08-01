a90004a5d355f7298419ea1cb139fc21594b32a1
Galina Psch <over.passover@gmail.com>
2020-08-01 03:03:46 +0300

    localstorage

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,21 +7,40 @@ const handleAuthError = (req, res, next) => {
   next(new UnauthorizedError('Необходима авторизация'));
 };
 
+// cookies
+
+// module.exports = (req, res, next) => {
+//   if (!req.cookies.jwt) {
+//     return handleAuthError(req, res, next);
+//   }
+
+//   const token = req.cookies.jwt;
+//   let payload;
+
+//   try {
+//     payload = jwt.verify(token, JWT_SECRET);
+//   } catch (err) {
+//     return handleAuthError(req, res, next);
+//   }
+//   req.user = payload;
+//   return next();
+// };
+
+// local storage
 module.exports = (req, res, next) => {
-  if (!req.cookies.jwt) {
-    return handleAuthError(req, res, next);
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return res.status(401).send({ message: 'Необходима авторизация' });
   }
 
-  const token = req.cookies.jwt;
+  const token = authorization.replace('Bearer ', '');
   let payload;
-
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     return handleAuthError(req, res, next);
   }
-
   req.user = payload;
-
   return next();
 };
